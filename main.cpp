e3e77da65834f2d7fdef0ff19ae66e99047c8ae5
rpersons1 <rpersons1@mail.csuchico.edu>
2018-04-12 23:02:24 -0700

    made a scorboard

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main()
 {
   int periods;
   int teams;
+  //vector <vector <int> > score;
 
   cout<<"How many competitors? ";
   cin>>teams;
@@ -21,15 +22,32 @@ int main()
   if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
       teams > MAX_TEAMS || periods > MAX_PERIODS )
   {
-    cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
     cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
     return 0;
   }
   else
   {
-   //make scoreboard and fill it with zeros
-  
-   //once created, display the scoreboard
+  vector <vector <int> > score(teams);
+  cout<<"SCOREBOARD\n";
+  for( int tm = 0; tm < teams; tm++ )
+  {
+     score[tm].resize( periods );
+     cout<<"Player "<<tm + 1<<": ";
+     for( int per = 0; per < periods; per++ )
+    {
+   // cout<<"1 ";
+//      cout<<"r-"<<row<<" "<<"c-"<<col<<"|";
+      score[tm][per] = 0;
+      cout<<score[tm][per]<<"|";
+    }
+    cout<<endl;
   }
+
+  //make scoreboard and fill it with zeros
+  
+  //once created, display the scoreboard
+  }
+
   return 0;
+
 }
